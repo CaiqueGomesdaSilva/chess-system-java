8497d162b4657199cbd8717a318f5a899222338f
CaiqueGomesdaSilva <@caique.gsilva@sptech.school>
2023-03-21 13:49:57 -0300

    first class: Position

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,12 +4,16 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author Caiqu
  */
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        Position poss = new Position(3, 5);
+        System.out.println(poss);
     }
 }
